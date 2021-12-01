c046d0b97ee2106bc9a71ce87086560ea2032172
Sergey Nikitin <srg.post@gmail.com>
2021-12-01 15:43:04 +0700

    aoc: 2021 day1

---
+++ src/adventofcode/2021/day1/common.ts
@@ -0,0 +1,49 @@
+export const readFile = async (): Promise<string[]> => {
+  const text = await Deno.readTextFile("./task.txt");
+  const lines = text.split("\n");
+  lines.pop();
+  return lines;
+};
+
+type Trend = "up" | "down" | "stay";
+
+const comparator = (
+  lines: string[],
+  start: number,
+  getWindow: (lines: string[], start: number) => number | null
+): Trend => {
+  const left = getWindow(lines, start);
+  const right = getWindow(lines, start + 1);
+  if (left === right || right === null || left === null) {
+    return "stay";
+  }
+  if (left > right) {
+    return "up";
+  }
+  return "down";
+};
+
+export const getResult = (
+  lines: string[],
+  getWindow: (input: string[], state: number) => number | null
+): number => {
+  const res = lines.reduce<{
+    ups: number;
+    downs: number;
+  }>(
+    (acc, _, ind) => {
+      const compared = comparator(lines, ind, getWindow);
+
+      return {
+        ups: acc.ups + (compared === "up" ? 1 : 0),
+        downs: acc.downs + (compared === "down" ? 1 : 0),
+      };
+    },
+    {
+      ups: 0,
+      downs: 0,
+    }
+  );
+
+  return res.downs;
+};
