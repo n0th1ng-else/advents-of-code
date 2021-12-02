defb976bbe379baf854de1ec5f034356878f2dcd
Sergey Nikitin <srg.post@gmail.com>
2021-12-02 12:09:23 +0700

    aoc: day2

--- src/adventofcode/2021/day1/task2.ts
+++ src/adventofcode/2021/day1/task2.ts
@@ -1,6 +1,10 @@
-import { getResult, readFile } from "./common.ts";
+/**
+ * deno run -A ./src/adventofcode/2021/day1/task2.ts
+ */
+import { getResult } from "./common.ts";
+import { readFile } from "../../common.ts";
 
-const rows = await readFile();
+const rows = await readFile(1);
 
 const getWindow = (lines: string[], start: number): number | null => {
   const size = lines.length;
