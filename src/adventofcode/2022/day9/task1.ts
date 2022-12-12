0421618a299a8bb82be3575fdbf620d26b63ba27
Sergey Nikitin <srg.post@gmail.com>
2022-12-13 00:30:30 +0100

    aoc(2022): Day 9

---
+++ src/adventofcode/2022/day9/task1.ts
@@ -0,0 +1,8 @@
+/**
+ * npm run aoc 2022 9 1
+ */
+import { approximateRope } from "./common.ts";
+
+const ROPE_LENGTH = 2;
+
+export const task = approximateRope(ROPE_LENGTH);
---
+++ src/adventofcode/2022/day9/task2.ts
@@ -0,0 +1,8 @@
+/**
+ * npm run aoc 2022 9 2
+ */
+import { approximateRope } from "./common.ts";
+
+const ROPE_LENGTH = 10;
+
+export const task = approximateRope(ROPE_LENGTH);
